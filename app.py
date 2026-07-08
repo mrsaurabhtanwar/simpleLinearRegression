46b7e9c995d514094544b476646ba1b0fd65039a
mrsaurabhtanwar <saurabhtanwar7320@gmail.com>
2026-07-08 14:07:18 +0530

    fixing the pytest path

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ import joblib
 import numpy as np
 import time
 import logging
-import mysql.connector
+# import mysql.connector
+from pathlib import Path
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -22,9 +23,13 @@ from pydantic import BaseModel
 app = FastAPI(title="Height Predication Service")
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "linear_regression_model.joblib"
+SCALER_PATH = BASE_DIR / "standard_scaler.joblib"
+
 try:
-    model = joblib.load('linear_regression_model.joblib')
-    scaler = joblib.load('standard_scaler.joblib')
+    model = joblib.load(MODEL_PATH)
+    scaler = joblib.load(SCALER_PATH)
 except Exception as e:
     logging.error(f"Failed to load the model artificates : {e}")
 
@@ -33,32 +38,32 @@ class InferenceInput(BaseModel):
     weight_lbs: float
 
 
-def save_predication_to_db(weight: float, predication: float, latency: float):
-    try:
-        # connect to XAMPP mysql 
-        connection = mysql.connector.connect(
-            host="host.docker.internal",
-            user="root",
-            password="",
-            database="mlops_db"
-        )
-        cursor = connection.cursor()
-        # sql raw query
-        sql_query = """
-            INSERT INTO predication_logs (weight_lbs, predicated_height_inches, latency_seconds) VALUES (%s, %s, %s)
-        """
-        # Execute the query safely using placeholders to prevent SQL injection
-        cursor.execute(sql_query, (weight, predication, latency))
+# def save_predication_to_db(weight: float, predication: float, latency: float):
+    # try:
+    #     # connect to XAMPP mysql 
+    #     connection = mysql.connector.connect(
+    #         host="host.docker.internal",
+    #         user="root",
+    #         password="",
+    #         database="mlops_db"
+    #     )
+    #     cursor = connection.cursor()
+    #     # sql raw query
+    #     sql_query = """
+    #         INSERT INTO predication_logs (weight_lbs, predicated_height_inches, latency_seconds) VALUES (%s, %s, %s)
+    #     """
+    #     # Execute the query safely using placeholders to prevent SQL injection
+    #     cursor.execute(sql_query, (weight, predication, latency))
 
-        # commit the transcation to save it permanently
-        connection.commit()
+    #     # commit the transcation to save it permanently
+    #     connection.commit()
 
-        cursor.close()
-        connection.close()
-        logging.info("Successfully logged predication to MySQL database.")
+    #     cursor.close()
+    #     connection.close()
+    #     logging.info("Successfully logged predication to MySQL database.")
 
-    except Exception as e:
-        logging.error(f"Database logging error : {e}")
+    # except Exception as e:
+    #     logging.error(f"Database logging error : {e}")
 
 
 @app.post("/predict")
@@ -80,11 +85,11 @@ def predict_height(data: InferenceInput):
         latency = time.time() - start_time
 
         # Save the workflow execution to your database
-        save_predication_to_db(
-            weight=data.weight_lbs,
-            predication=round(predict_height, 2),
-            latency=round(latency, 4)
-        )
+        # save_predication_to_db(
+        #     weight=data.weight_lbs,
+        #     predication=round(predict_height, 2),
+        #     latency=round(latency, 4)
+        # )
 
         return {
             "Predicted_height_inches": round(predict_height, 2),
